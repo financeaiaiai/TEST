593d6df4d25390dcd10a3f87b00eb37053a2c2b7
ting <34786690+financeaiaiai@users.noreply.github.com>
2018-09-30 23:36:47 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#!coding:utf-8
+'''
+author: dinghewu
+date: 2018/9/30 @ 22:37
+python 3.6.2
+'''
+class hw_base(object):
+    '''
+    this is a test
+    '''
+    def __init__(self):
+        pass
+    def data_pre(self):
+        pass
+    def exec(self):
+        pass
+    def show(self):
+        pass
+
